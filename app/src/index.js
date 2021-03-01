3ea019b3a13136b3b99f04544d1e35f5cfe9ec6a
Ziyad Al Obaidi <ziyadalobaidi@Ziyads-MBP.attlocal.net>
2021-03-01 02:32:38 -0500

    init react app

---
+++ app/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+// testing graphql server
+client
+  .query({
+    query: gql`
+      query {
+        favoriteGists {
+          id
+          createdOn
+          favorite
+          files {
+            filename
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
